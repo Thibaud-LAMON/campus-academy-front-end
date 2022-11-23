90b91cc5f933ed4783b7cdd8041f568760b564c5
Thibaud-LAMON <thib.lamon@gmail.com>
2022-11-23 16:08:04 +0100

    adding thumbnail + description

--- front-end-lesson/src/App.js
+++ front-end-lesson/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './header/Header.js';
+import Thumbnail from './thumbnail/Thumbnail';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Thumbnail />
     </div>
   );
 }
---
+++ front-end-lesson/src/thumbnail/Thumbnail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './thumbnail.css'
+
+function Thumbnail() {
+  return (
+    <div className='Thumbnail'>
+        <p className='titre'>TITRE</p>
+        <p className='desc'>Desription : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nunc risus, vulputate a massa id, ultricies mollis orci. In lacus diam, malesuada et suscipit ut, semper eu est. Sed ac diam sed risus accumsan consectetur. Sed nec malesuada lacus. Proin tempor eros nec mauris auctor, et gravida elit dictum. Ut at convallis nisl, et placerat velit. Aliquam facilisis nisl ac ex blandit, nec fermentum orci maximus. Duis elementum porta rhoncus.</p>
+        
+    </div>
+  )
+}
+
+export default Thumbnail
